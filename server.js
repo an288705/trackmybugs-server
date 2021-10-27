507a28dc38088a5682ff9ebf000fe347df3a7b46
an288705 <aj0466012@gmail.com>
2021-10-26 23:12:24 -0400

    test

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const app = express();
 
+//from mac
+
 mongoose.connect(process.env.DB_URL,{useNewUrlParser : true, useUnifiedTopology : true},(err)=>{
     if(!err) return console.log('connected to DB')
 
